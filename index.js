919102ca96e6b8921607db98dff801ea0cc276bb
CarmenViet <149909827+CarmenViet@users.noreply.github.com>
2024-04-27 21:27:10 -0500

    finished wabesite

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+alreadyClicked = false
+keysPressed = []
 function imageClick() {
-    if (! this.alreadyClicked)
+    if (!alreadyClicked)
     {
       addimage();
-      this.alreadyClicked = true;
+      alreadyClicked = true;
     }
   }
   
-  function addimage() {
+function addimage() {
     var img = document.createElement("img");
     img.src = "wabe_love.png";
+    img.id = "wabe_love"
     img.onclick = imageClick;
     document.body.appendChild(img);
   }
+
+document.addEventListener('keydown', 
+  function(event) {
+    keysPressed.push(event.code);
+    let lastFourPressed = keysPressed.slice(-4);
+    if (lastFourPressed[0] == "KeyW" && lastFourPressed[1] == "KeyA" && lastFourPressed[2] == "KeyB" && lastFourPressed[3] == "KeyE")
+    {
+      wabe.width = "1100";
+      wabe.height = "800";
+    }
+  }
+)
